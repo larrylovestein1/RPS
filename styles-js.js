c1c989197ef169906ad2ca322d91bbcba7369834
maxum <larrylovesteinburner@gmail.com>
2022-11-12 19:16:43 -0600

    i externally linked my js file and inside of it i created my first function to generate a randome answer of rock paper or scissors

---
+++ styles-js.js
@@ -0,0 +1,14 @@
+//game 
+
+function getComputerChoice(){
+ let randomNumber = Math.floor(Math.random() * 3)
+   switch (randomNumber){
+    case 0: 
+      return 'ROCK'
+    case 1:
+        return 'PAPER'
+    case 2: 
+        return 'SCISSORS'
+
+   }
+}
