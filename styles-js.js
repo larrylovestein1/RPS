49291aa4469b6ac511ac3a2be860d3b48d5d9d35
maxum <larrylovesteinburner@gmail.com>
2023-01-22 14:26:34 -0600

    I cannot figure out some of the code on this and it has cost me weeks in agony , I accept this as the final result lmao. Hello potential future employers.

--- styles-js.js
+++ styles-js.js
@@ -4,20 +4,23 @@ function getComputerChoice(){
    let randomNumber = Math.floor(Math.random()*3)
    switch (randomNumber){
        case 0:
-         return 'ROCK'
+         return 'ROCK';
+         break;
        case 1: 
-         return 'PAPER'
+         return 'PAPER';
+         break;
        case 2:
-           return 'SCISSORS'
+           return 'SCISSORS';
+           break;
    }
 }
 
-function playRound (playerSelection,computerSelection){
+function playRound (playerSelection){
    //the first if statement will check to see if it a tie game 
    if (
       (computerSelection === playerSelection)
-     ) {
-        return "tie game"
+     )  {
+        return "tie game";
      }
 
      else
@@ -27,7 +30,8 @@ function playRound (playerSelection,computerSelection){
        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') || // checks all possible outcomes , returns winner of round as player
        (playerSelection === 'PAPER' && computerSelection === 'ROCK')
        ) {
-         return 'player beat the machine!'
+         playerPoints.textContent = 'Player:' + ++playerScore 
+         return 'player beat the machine!';
        }
      else
    if (
@@ -35,41 +39,36 @@ function playRound (playerSelection,computerSelection){
       (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') || // checks all possible outcomes for computers answer returns winner of the round as computer
       (computerSelection === 'PAPER' && playerSelection ==='ROCK')
       ) {
-         return 'the machine wins!'
+        computerPoints.textContent = 'Computer:' + ++computerScore
+        return 'the machine wins!';
       }  
       }
 
-     
-     //trying out loops with the game() function that plays five rounds , then adding something to keep track of score and announce winner after
-     // i get the function working properly...
 
-     
-     function game(){
-      //starts to loop the playRound function
-      for(let i = 0; i < 5; i++){
-        //calls playRound function and redefines the values above to fight scope conflicts.
-          //passing in newly returned values from playerround and computerselection
-          const computerSelection = getComputerChoice();
-          const playerSelection = 'ROCK';
-          const currentRound = playRound(playerSelection,computerSelection);
-          //log our results
-          console.log(currentRound);
-      }
-  
-  }
+    
 
-
-
- 
-
-
-
-// i made a temporary answer value for the player later i will add in a input value so that i can choose my value 
-      const playerSelection = prompt('Choose your weapon!');
+      let computerScore = 0;
+      let playerScore = 0;
+      const playerPoints = document.querySelector('#player');
+      const computerPoints = document.querySelector('#computer');
       const computerSelection = getComputerChoice();          
-      console.log(game());
+      const rock = document.querySelector('#rock');
+      const paper = document.querySelector('#paper');
+      const scissors = document.querySelector('#scissors');
 
+      rock.addEventListener('click', () => {
+        console.log (playRound('ROCK', computerSelection));
+         });
 
+      paper.addEventListener('click', () => {
+        console.log(playRound('PAPER',computerSelection));
+      });
+
+      scissors.addEventListener('click', () => {
+        console.log(playRound('SCISSORS', computerSelection));
+      });
+
+    
 
 
 
